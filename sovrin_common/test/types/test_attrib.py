0c28481176acb0dfacf9a1cfa7a0e5c22393abde
Alexander Shekhovtsov <alexander.shekhovcov@dsr-company.com>
2017-05-25 15:23:02 +0300

    add test for attrib validator

--- sovrin_common/test/types/test_attrib.py
+++ sovrin_common/test/types/test_attrib.py
@@ -11,7 +11,8 @@ validator = ClientAttribOperation()
 
 def test_attrib_with_enc_raw_hash_in_same_time_fails():
     txn = {
-        TXN_TYPE: ATTRIB, TARGET_NYM: 'foo',
+        TXN_TYPE: ATTRIB,
+        TARGET_NYM: 'foo',
         RAW: '{}',
         ENC: 'foo',
         HASH: 'bar'
@@ -24,9 +25,22 @@ def test_attrib_with_enc_raw_hash_in_same_time_fails():
 
 def test_attrib_without_enc_raw_hash_fails():
     txn = {
-        TXN_TYPE: ATTRIB, TARGET_NYM: 'foo'
+        TXN_TYPE: ATTRIB,
+        TARGET_NYM: 'foo'
     }
     with pytest.raises(TypeError) as ex_info:
         validator.validate(txn)
     ex_info.match("validation error: missed fields '{}, {}, {}'"
+                  "".format(RAW, ENC, HASH))
+
+
+def test_attrib_raw_is_invalid_json():
+    txn = {
+        TXN_TYPE: ATTRIB,
+        TARGET_NYM: 'foo',
+        RAW: 'foo',
+    }
+    with pytest.raises(TypeError) as ex_info:
+        validator.validate(txn)
+    ex_info.match("validation error: should be valid JSON string"
                   "".format(RAW, ENC, HASH))
