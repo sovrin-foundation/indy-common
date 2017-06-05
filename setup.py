e85ce8c66e8d290366964cc1dc31d5884e218a6d
Victor Muzychenko <mzkvct@gmail.com>
2017-06-05 14:23:22 +0300

    post merge fixes

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ class EnhancedInstallDev(develop):
 
 
 setup(
-    name='sovrin-common-dev',
+    name='sovrin-common',
     version=__version__,
     description='Sovrin common',
     url='https://github.com/sovrin-foundation/sovrin-common.git',
@@ -81,7 +81,7 @@ setup(
         '': ['*.txt', '*.md', '*.rst', '*.json', '*.conf', '*.html',
              '*.css', '*.ico', '*.png', 'LICENSE', 'LEGAL', '*.sovrin']},
     include_package_data=True,
-    install_requires=['plenum-dev==0.3.134'],
+    install_requires=['plenum==0.3.15'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     scripts=['scripts/get_keys',
